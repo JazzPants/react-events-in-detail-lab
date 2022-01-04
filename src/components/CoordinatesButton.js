7dad94276cbe76526780eb3832daedc925705ec1
JazzPants <leighton.l14@gmail.com>
2022-01-04 21:59:41 +1100

    Done!

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,23 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+class CoordinatesButton extends Component {
+    constructor(props) {
+        super();
+    }
+
+    handleClick = (e) => {
+        this.props.onReceiveCoordinates([e.clientX, e.clientY])
+        // return [e.clientX, e.clientY]
+    }
+//onReceiveCoordinates is a prop(attribute) arrow function from index.js , it is a prop passed to this component. 
+//We use an event handler to pass some event data to that function mouseCoordinates is "replaced" by [e.clientX, e.clientY]!!!
+    render() {
+        return (
+            <button onClick={this.handleClick}>Co-ordinates</button>
+        )
+    }
+
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,32 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+class DelayedButton extends Component {
+    // constructor(props) {
+    //     super();
+    // }
+
+    handleClick = (event) => {
+        event.persist();
+        // event.preventDefault();
+        //or store a variable: 
+        // const eventObj = event;
+        // const eventObj = e.target; //will print the actual DOM element, target is the DOM element "targetted" by the user event
+        setTimeout(() => {
+            this.props.onDelayedClick(event) //pass the event click to onDelayedClick, e or e.target
+        }, this.props.delay)
+
+    }
+
+//e.persist() passes the test
+//goes null if we use const eventObj = e;
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>Delayed BUTTON</button>
+        )
+    }
+
+}
+
+export default DelayedButton;
